81e1f866695d9848c75c734c3f5a4a073d4b2e6b
Marc Augier <m.augier@me.com>
2021-09-27 18:11:05 +0200

    ajout d'un backend strapi

---
+++ back-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0e81ab8404e22dbb891197d4b4f8d280'),
+    },
+  },
+});
---
+++ back-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b8ec2e6-7fae-4665-aedf-284d8ee0ac71'
+};
